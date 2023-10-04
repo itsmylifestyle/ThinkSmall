8baeadfb61efcbdbd63e9b99717ad42d06606fe9
itsmylifestyle <hahaii00@mail.ru>
2023-10-04 18:33:55 +0600

    some small changes

---
+++ SnapchatClone/ViewControllers/SignInVC.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Айбек on 04.10.2023.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+    
+    
+    @IBOutlet weak var emailInput: UITextField!
+    
+    @IBOutlet weak var usernameInput: UITextField!
+    
+    
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SignInAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func SignUpAction(_ sender: Any) {
+    }
+    
+}
+
---
+++ SnapchatClone/ViewControllers/Upload.swift
@@ -0,0 +1,36 @@
+//
+//  Upload.swift
+//  SnapchatClone
+//
+//  Created by Айбек on 04.10.2023.
+//
+
+import UIKit
+
+class UploadVC: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func uploadButton(_ sender: Any) {
+    }
+    
+
+}
