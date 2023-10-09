b0ba7e728c789fed7806f6df1e8dde85f56fca1a
itsmylifestyle <hahaii00@mail.ru>
2023-10-10 00:23:37 +0600

    some functions

--- SnapchatClone/ViewControllers/Settings.swift
+++ SnapchatClone/ViewControllers/Settings.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingsVC: UIViewController {
 
@@ -14,19 +15,14 @@ class SettingsVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction func logOut(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toSignVC", sender: nil)
+        } catch {
+            
+        }
     }
     
     
--- SnapchatClone/ViewControllers/SignInVC.swift
+++ SnapchatClone/ViewControllers/SignInVC.swift
@@ -6,15 +6,13 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInVC: UIViewController {
     
     
     @IBOutlet weak var emailInput: UITextField!
-    
     @IBOutlet weak var usernameInput: UITextField!
-    
-    
     @IBOutlet weak var passwordInput: UITextField!
     
     override func viewDidLoad() {
@@ -23,11 +21,46 @@ class SignInVC: UIViewController {
     }
 
     @IBAction func SignInAction(_ sender: Any) {
+        if usernameInput.text! != "" && passwordInput.text! != "" && emailInput.text! != "" {
+            Auth.auth().signIn(withEmail: self.emailInput.text!, password: self.passwordInput.text!) { res, error in
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                } else {
+                    self.performSegue(withIdentifier: "FeedVCsegue", sender: nil)
+                }
+            }
+        } else {
+            self.makeAlert(title: "Error", message: "Username or password is missing")
+        }
     }
     
     
     @IBAction func SignUpAction(_ sender: Any) {
+        if usernameInput.text! != "" && passwordInput.text! != "" && emailInput.text! != "" {
+            Auth.auth().createUser(withEmail: emailInput.text!, password: passwordInput.text!) { auth, error in
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                } else {
+                    let firestoreMy = Firestore.firestore()
+                    let userDict = ["email" : self.emailInput.text!, "username" : self.usernameInput.text!] as [String : Any]
+                    firestoreMy.collection("UserInfo").addDocument(data: userDict) { error in
+                        if error != nil {
+                            self.makeAlert(title: "Error", message: "Connection may be lost")
+                        }
+                    }
+                    self.performSegue(withIdentifier: "FeedVCsegue", sender: nil)
+                }
+            }
+        } else {
+            self.makeAlert(title: "Error", message: "Username or password is missing")
+        }
     }
     
+    func makeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let ok = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel)
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
