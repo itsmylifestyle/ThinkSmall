0ad84eb92e1f140dac1fe0b3ecff58336d270d05
itsmylifestyle <hahaii00@mail.ru>
2023-10-13 02:55:15 +0600

    .

--- SnapchatClone/ViewControllers/Upload.swift
+++ SnapchatClone/ViewControllers/Upload.swift
@@ -50,14 +50,40 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate,UINavigationCo
                         if error == nil {
                             let imageUrl = url?.absoluteString
                             let firestore = Firestore.firestore()
-                            let snapDictionary = ["imageUrl" : imageUrl!, "snapOwner" : UserSingleton.sharedUserInfo.username, "date" : FieldValue.serverTimestamp()] as [String : Any]
-                            firestore.collection("Snaps").addDocument(data: snapDictionary) { error in
+                            
+                            
+                            firestore.collection("Snaps").whereField("snapOwner", isEqualTo: UserSingleton.sharedUserInfo.username).getDocuments { snap, error in
                                 if error != nil {
                                     self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
                                 } else {
-                                    self.tabBarController?.selectedIndex = 0
-                                    self.imageView.image = UIImage(named: "folder.badge.plus")
-                                }
+                                    if snap?.isEmpty == false && snap != nil {
+                                        for doc in snap!.documents {
+                                            let docId = doc.documentID
+                                            if var imgUrlArray = doc.get("imgUrlArray") as? [String] {
+                                                imgUrlArray.append(imageUrl!)
+                                                
+                                                let additionalDict = ["imgUrlArray" : imgUrlArray] as [String : Any]
+                                                
+                                                firestore.collection("Snaps").document(docId).setData(additionalDict, merge: true) { error in
+                                                    if error == nil {
+                                                        self.tabBarController?.selectedIndex = 0
+                                                        self.imageView.image = UIImage(named: "folder.badge.plus")
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    } else {
+                                        let snapDictionary = ["imgUrlArray" : [imageUrl!], "snapOwner" : UserSingleton.sharedUserInfo.username, "date" : FieldValue.serverTimestamp()] as [String : Any]
+                                        firestore.collection("Snaps").addDocument(data: snapDictionary) { error in
+                                            if error != nil {
+                                                self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                                            } else {
+                                                self.tabBarController?.selectedIndex = 0
+                                                self.imageView.image = UIImage(named: "folder.badge.plus")
+                                            }
+                                        }
+                                    }//there is error in firebase when I save more than 1 image
+                                }    //fix it tomorrow
                             }
                         }
                     }
